5de4681cd6c00593c3215590574495831a80def7
nayoung <nayoung021008@naver.com>
2024-03-27 09:50:47 +0900

    두번째 과제

--- 2024frontend/index.js
+++ 2024frontend/index.js
@@ -1,11 +1,23 @@
-const button1 = document.querySelector("#추가버튼");
-const input1 = document.querySelector("#입력값");
+const button = document.querySelector("#추가버튼");
+const input = document.querySelector("#입력창");
 const todos = document.querySelector("#할일들");
 
-button1.addEventListener("click", () => {
-  const value1 = input1.value;
-  const p1 = document.createElement("p");
-  p1.innerText = value1;
-  todos.appendChild(p1);
-  input1.value = null;
+button.addEventListener("click", () => {
+  //input에 있는 입력값들 가져오기
+  const value = input.value;
+
+  //입력값을 바탕으로 새로운 할일 만들기
+  //새로운 할 일 표시용 <p>가 필요함.
+  const p = document.createElement("p");
+  p.innerText = value;
+  todos.appendChild(p);
+  input.value = ""; //입력창의 값 비워주기
+
+  p.addEventListener("click", () => {
+    if (p.className !== "done") {
+      p.className = "done";
+    } else {
+      p.className = "";
+    }
+  });
 });
