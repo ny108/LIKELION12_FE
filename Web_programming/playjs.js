a55a0d137d00fdd2764072fda087860b10bce714
nayoung <nayoung021008@naver.com>
2024-04-02 13:33:55 +0900

    feat: 5주차-1 실습

---
+++ Web_programming/playjs.js
@@ -0,0 +1,6 @@
+function calc() {
+  var a = document.getElementById("x").value;
+  var b = document.getElementById("y").value;
+  var s = document.getElementById("sum");
+  s.value = Number(a) + Number(b);
+}
